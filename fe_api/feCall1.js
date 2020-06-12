7b56e029574e302b7b8cda32e13d41ac68f2dafd
David Law <davidlaw@ZILUs-Mac-mini.local>
2020-06-12 11:34:38 +0800

    add nosql

--- fe_api/feCall1.js
+++ fe_api/feCall1.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
 
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-2'});
+var db = new AWS.DynamoDB();
+
 router.get('/call1', function (req, res) {
 
-    res.send("return nothing 1");
+   var userId = Math.random();
+   var d = new Date();
+   var n = d.toISOString();
+   var params = {
+    TableName: 'cf_income_raw',
+    Item: {
+      'userId' : {S: userId},
+      'amountBuy': {
+        "N": "999"
+      },
+      "amountSell": {
+        "N": "1000"
+      },
+      "currencyFrom": {
+        "S": "EUR"
+      },
+      "currencyTo": {
+        "S": "GBP"
+      },
+      "originatingCountry": {
+        "S": "FR"
+      },
+      "rate": {
+        "N": "0.7471"
+      },
+      "timePlaced": {
+        "S": n
+      },
+    }
+  };
+  
+  // Call DynamoDB to add the item to the table
+  db.putItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+      res.send("return err");
+    } else {
+      console.log("Success", data);
+      res.send("return success");
+    }
+  });
+    
+
 
 });
 
